fd5b073c96da83f5929020d999d958f2efa0331c
ionm04 <ion2004@stanford.edu>
2025-06-06 22:16:45 -0700

    Update test_gpt_base.py

--- tests/PTSD/test_gpt_base.py
+++ tests/PTSD/test_gpt_base.py
@@ -177,7 +177,7 @@ def main() -> None:
             continue
         name = pid
         if "va_pruned" in transcript_dir:
-            name = pid + "_va_pruned_"
+            name = f"{pid}_va_pruned_"
         transcript_path = os.path.join(transcript_dir, f"{name}_Transcript.csv")
         if not os.path.exists(transcript_path):
             print(f"{transcript_path} does not exist")
